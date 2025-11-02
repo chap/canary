b8de730ff7b61ed76171e1e6d63631e92e406148
Chap Ambrose <cambrose@salesforce.com>
2025-11-02 22:52:01 +0000

    Fix: replace 'any' method with compatible route handlers for all HTTP methods

--- app.rb
+++ app.rb
@@ -4,8 +4,12 @@ require 'json'
 set :port, ENV['PORT'] || 4567
 set :server, 'puma'
 
-any '*' do
+def canary_response
   content_type :json
   { canary: 'canary' }.to_json
+end
+
+%w[get post put delete patch options head].each do |method|
+  send(method, '*') { canary_response }
 end
 
